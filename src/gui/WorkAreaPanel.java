538db6b42c9443c043acc72537b490eabf0bc698
JSFernandes <jsfernandes91@gmail.com>
2013-12-28 18:37:17 +0000

    Added arrowheads
    :3

--- src/gui/WorkAreaPanel.java
+++ src/gui/WorkAreaPanel.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.geom.AffineTransform;
 import java.util.Hashtable;
 
 import javax.swing.JPanel;
@@ -16,6 +19,8 @@ public class WorkAreaPanel extends JPanel implements FocusListener{
 	public Hashtable<Integer, Point> rectangles = new Hashtable<Integer, Point>();
 	public Hashtable<Integer, Point[]> lines = new Hashtable<Integer, Point[]>();
 	
+	private final int ARR_SIZE = 10;
+	
 	public void repaint() {
 		
 		if(rectangles != null) {
@@ -26,7 +31,8 @@ public class WorkAreaPanel extends JPanel implements FocusListener{
 		
 		if(lines != null) {
 			for(Point[] p : lines.values()) {
-				getGraphics().drawLine(p[0].x, p[0].y, p[1].x, p[1].y);
+				//getGraphics().drawLine(p[0].x, p[0].y, p[1].x, p[1].y);
+				drawArrow(getGraphics(), p[0].x, p[0].y, p[1].x, p[1].y);
 			}
 		}
 	}
@@ -40,4 +46,20 @@ public class WorkAreaPanel extends JPanel implements FocusListener{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	void drawArrow(Graphics g1, int x1, int y1, int x2, int y2) {
+        Graphics2D g = (Graphics2D) g1.create();
+
+        double dx = x2 - x1, dy = y2 - y1;
+        double angle = Math.atan2(dy, dx);
+        int len = (int) Math.sqrt(dx*dx + dy*dy);
+        AffineTransform at = AffineTransform.getTranslateInstance(x1, y1);
+        at.concatenate(AffineTransform.getRotateInstance(angle));
+        g.transform(at);
+
+        // Draw horizontal arrow starting in (0, 0)
+        g.drawLine(0, 0, len, 0);
+        g.fillPolygon(new int[] {len, len-ARR_SIZE, len-ARR_SIZE, len},
+                      new int[] {0, -ARR_SIZE, ARR_SIZE, 0}, 4);
+    }
 }
